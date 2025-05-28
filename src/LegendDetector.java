aad5d61cdd7e348ff8b3a4f5bed91f37b89d1739
Matthew Raburn <matthew.raburn@gmail.com>
2025-05-27 21:56:18 -0400

    more in legend detector

--- src/LegendDetector.java
+++ src/LegendDetector.java
@@ -23,8 +23,48 @@ public class LegendDetector {
      * @return LegendInfo containing detected colors and their locations
      */
     public LegendInfo detectLegendColors(BufferedImage bufferedImage) {
-        // Convert image to OpenCV Mat
-        Mat imageMat = bufferedImage(bufferedImage);
+        // Convert BufferedImage to OpenCV Mat
+        Mat imageMat = bufferedImageToMat(bufferedImage);
+
+        // Create result object
+        LegendInfo legendInfo = new LegendInfo();
+
+        // PSEUDOCODE:
+        // 1. Apply image processing to identify potential legend areas
+        // 2. Detect similarly sized color regions
+        // 3. Extract colors from those regions
+        // 4. Add results to LegendInfo object
+
+        // Step 1: Convert to appropriate color space for processing
+        Mat hsvImage = new Mat();
+        Imgproc.cvtColor(imageMat, hsvImage, Imgproc.COLOR_BGR2HSV);
+
+        // Step 2: Find potential legend regions
+        // This is a basic implementation - we'll look for contours of similar size/shape
+        List<MatOfPoint> contours = findPotentialLegendElements(imageMat);
+
+        // Step 3: Filter contours to find likely legend color squares/circles
+        List<MatOfPoint> legendContours = filterLegendContours(contours);
+
+        // Step 4: Extract colors and add to our result
+        for (MatOfPoint contour : legendContours) {
+            // Get bounding rectangle
+            Rect rect = Imgproc.boundingRect(contour);
+
+            // Extract mean color from this region
+            Scalar meanColor = extractMeanColor(imageMat, rect);
+
+            // Convert OpenCV Scalar to Java Color
+            Color javaColor = new Color((int)meanColor.val[2],
+                    (int)meanColor.val[1],
+                    (int)meanColor.val[0]);
+
+            // Add to our legend info
+            legendInfo.addLegendColor(javaColor,
+                    new Rectangle(rect.x, rect.y, rect.width, rect.height));
+        }
+
+        return legendInfo;
 
     }
 
